06756e3ff9c4348f54fb7a5d405e949a242a19ce
eryczou <eryczou@gmail.com>
2021-06-12 23:09:13 -0400

    fix

--- src/pages/home/workflow-feature-section/styled.js
+++ src/pages/home/workflow-feature-section/styled.js
@@ -13,6 +13,7 @@ export const WorkflowFeatureWrapper = styled(Grid)`
     padding-right: 16px;
   }
   border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+  overflow-x:hidden;
 `;
 
 export const FeatureTypographyWrapper = styled(Typography)`
